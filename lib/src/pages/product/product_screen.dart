2e745ea306822b406ff708088aa9989a46253396
Wkerlyson Batista <wkerlyson.batista@gmail.com>
2022-09-12 19:23:13 -0300

    Add to cart button and Hero animation

--- lib/src/pages/product/product_screen.dart
+++ lib/src/pages/product/product_screen.dart
@@ -16,78 +16,116 @@ class ProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white.withAlpha(230),
-      body: Column(children: [
-        Expanded(
-          child: Image.asset(item.imgUrl),
-        ),
-        Expanded(
-          child: Container(
-            padding: const EdgeInsets.all(32),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(50),
+      body: Stack(
+        children: [
+          Column(children: [
+            Expanded(
+              child: Hero(
+                tag: item.imgUrl,
+                child: Image.asset(item.imgUrl),
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey,
-                  offset: Offset(0, 2),
-                )
-              ],
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                //Nome - QTD
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        item.itemName,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                          fontSize: 27,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Container(height: 30, width: 70, color: Colors.red),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(32),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(50),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(0, 2),
+                    )
                   ],
                 ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    //Nome - QTD
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            item.itemName,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 27,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Container(height: 30, width: 70, color: Colors.red),
+                      ],
+                    ),
 
-                //preço
-                Text(
-                  utilServices.priceToCurrency(item.price),
-                  style: TextStyle(
-                    fontSize: 23,
-                    fontWeight: FontWeight.bold,
-                    color: CustomColors.customSwatchColor,
-                  ),
-                ),
+                    //preço
+                    Text(
+                      utilServices.priceToCurrency(item.price),
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        color: CustomColors.customSwatchColor,
+                      ),
+                    ),
 
-                //Descrição
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: SingleChildScrollView(
-                      child: Text(
-                        item.description,
-                        style: const TextStyle(
-                          height: 1.5,
+                    //Descrição
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: SingleChildScrollView(
+                          child: Text(
+                            item.description,
+                            style: const TextStyle(
+                              height: 1.5,
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ),
 
-                //Botao
-                ElevatedButton(onPressed: () {}, child: Text('botão'))
-              ],
+                    //Botao
+                    SizedBox(
+                      height: 55,
+                      child: ElevatedButton.icon(
+                        onPressed: () {},
+                        label: const Text(
+                          'Add no carrinho',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        icon: const Icon(
+                          Icons.add_shopping_cart_rounded,
+                          color: Colors.white,
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ]),
+          Positioned(
+            left: 10,
+            top: 10,
+            child: SafeArea(
+              child: IconButton(
+                onPressed: () => Navigator.of(context).pop(),
+                icon: const Icon(Icons.arrow_back_ios),
+              ),
             ),
-          ),
-        )
-      ]),
+          )
+        ],
+      ),
     );
   }
 }
